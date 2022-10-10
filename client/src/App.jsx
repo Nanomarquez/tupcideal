2459aea52a49e5edcb04fb59ac0427c2c5fecd84
Nanomarquez <nanomarqueztres@gmail.com>
2022-10-10 12:46:41 -0300

    Prueba

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold">Hello world!</h1>
+      <h1 className="text-3xl font-bold">Hola</h1>
     </div>
   );
 }
