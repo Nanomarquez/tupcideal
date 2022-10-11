73fb1499a257ed6e4e56b75a49f173b2ea8381bf
FedeHuertas <federicohuertas@outlook.com>
2022-10-11 00:44:28 -0300

    Fix: Cambio define sequalize de cpu a nombre correcto

--- api/src/models/Case.js
+++ api/src/models/Case.js
@@ -1,7 +1,7 @@
 const { DataTypes } = require('sequelize');
 
 module.exports = (sequelize) => {
-  sequelize.define('CPU', {
+  sequelize.define('Case', {
     name: {
         type: DataTypes.STRING
     },
--- api/src/models/InternalHardDrive.js
+++ api/src/models/InternalHardDrive.js
@@ -1,7 +1,7 @@
 const { DataTypes } = require('sequelize');
 
 module.exports = (sequelize) => {
-  sequelize.define('CPU', {
+  sequelize.define('InternalHardDrive', {
     name: {
       type: DataTypes.STRING
     },
