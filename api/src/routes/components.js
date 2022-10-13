3a68994d8a55794722764b7b3773fbf3168e6254
FedeHuertas <federicohuertas@outlook.com>
2022-10-13 18:21:20 -0300

    Agregada la ruta /components/cpu
    Devuelve todos los cpu o sólo los intel o amd si se le pasa por query la marca como ?brand=intel p.e.

--- api/src/routes/components.js
+++ api/src/routes/components.js
@@ -1,8 +1,29 @@
 const { Router } = require('express');
 const router = Router();
+const { Op } = require("sequelize");
+const { CPU } = require('../db.js');
+const bulkCPU = require('../data/cpu.json')
 
-router.get('/', (req, res) => {
-    res.send('components route')
-})
+router.get('/cpu', async (req, res) => {
+    const brand = req.query.brand;
+
+    allCPU = await CPU.findAll();
+    !allCPU.length ? CPU.bulkCreate(bulkCPU) : null;
+    
+
+    if(brand) {
+        const filteredCPU = await CPU.findAll({
+            where: {
+                name: {
+                    [Op.iLike]: `${brand}%`
+                }
+            }
+        });
+
+        res.send(filteredCPU);
+    } else {
+        res.send(await CPU.findAll())
+    };
+});
 
 module.exports = router;
