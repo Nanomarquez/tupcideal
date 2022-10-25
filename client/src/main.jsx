c5a1703dc90c6f17b21f8a219caa12d7e275deec
Nanomarquez <nanomarqueztres@gmail.com>
2022-10-25 20:53:31 -0300

    HashRouter

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App";
 import "./index.css";
 import { Provider } from "react-redux";
 import store from "./redux/store";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import "swiper/css/bundle";
 import { AuthProvider } from "./context/authContext";
 import axios from 'axios';
@@ -21,9 +21,9 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthProvider>
       <Provider store={store}>
-        <BrowserRouter>
+        <HashRouter>
           <App />
-        </BrowserRouter>
+        </HashRouter>
       </Provider>
     </AuthProvider>
   </React.StrictMode>
